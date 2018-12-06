cb6dfb32c7c034949c382af0827ce06798e94281
Dan <danhemerlein@gmail.com>
2018-12-06 16:54:28 -0500

    add elements and styles to DetailsBlock

---
+++ src/components/DetailBlock/index.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from "react";
+import cx from "classnames";
+import "./DetailBlock.scss";
+
+class DetailBlock extends PureComponent {
+
+  render() {
+    return (
+      <div className={cx("DetailBlock flex flex-col items-center")}>
+        <div className={cx("DetailBlock__headline")}>
+          <h3 className={cx("my3 sans-serif bold text-center text-dark-blue ")}>{this.props.headline}</h3>
+        </div>
+        <div className={cx("DetailBlock__paragraph")}>
+          <p className={cx("sans-serif text-dark-blue")}>{this.props.paragraph}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DetailBlock;
--- src/views/MainView.js
+++ src/views/MainView.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import get from "utils/get";
 
 import Hero from "components/Hero";
+import DetailBlock from "components/DetailBlock";
 
 const MainView = ({ model }) => {
   if (!model || model.isError) return <h1>Something went wrong...</h1>;
@@ -11,6 +12,10 @@ const MainView = ({ model }) => {
       <Hero
         headline={get(model, "fields.headline", {})}
         subHeadline={get(model, "fields.subHeadline", {})}
+      />
+      <DetailBlock
+        headline={get(model, "fields.detailHeadline", {})}
+        paragraph={get(model, "fields.paragraph", {})}
       />
     </Fragment>
   );
