66cf18eac1843fbe4b7f07ffb60d83be162770b9
Dan <danhemerlein@gmail.com>
2018-12-07 11:53:45 -0500

    add styles to Hero and Details Block

--- src/components/DetailBlock/index.js
+++ src/components/DetailBlock/index.js
@@ -6,7 +6,7 @@ class DetailBlock extends PureComponent {
 
   render() {
     return (
-      <div className={cx("DetailBlock flex flex-col items-center")}>
+      <div className={cx("DetailBlock flex flex-col items-center pt8")}>
         <div className={cx("DetailBlock__headline")}>
           <h3 className={cx("my3 sans-serif bold text-center text-dark-blue ")}>{this.props.headline}</h3>
         </div>
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -18,8 +18,8 @@ class Hero extends PureComponent {
               <h3 className={cx("Hero__headline text-white sans-serif bold mb2")}>{this.props.headline}</h3>
               <h4 className={cx("Hero__sub-headline text-white sans-serif mb2")}>{this.props.subHeadline}</h4>
               <div className={cx("flex")}>
-                <button className={cx("Hero__button cf-btn-primary col-6 p_5")}>Start building free</button>
-                <button className={cx("Hero__button cf-btn-secondary col-6 p_5 ml1")}>Request demo</button>
+                <button className={cx("Hero__button cf-btn-primary col-6 py1 px_5")}>Start building free</button>
+                <button className={cx("Hero__button cf-btn-secondary col-6 py1 px_5 ml1")}>Request demo</button>
               </div>
             </div>
           </div>
